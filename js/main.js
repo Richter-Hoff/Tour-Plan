e9a3c3cf79b2bc7388f6c2044ede51aeb43c2ddf
Richter-Hoff <richterhoff@gmail.com>
2021-05-12 20:32:50 +0300

    create: added section reviews

--- js/main.js
+++ js/main.js
@@ -16,3 +16,21 @@ const hotelSwiper = new Swiper('.hotel-slider', {
     pageUpDown: true,
   }
 });
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Optional parameters
+  loop: true,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.reviews-slider__button--next',
+    prevEl: '.reviews-slider__button--prev',
+  
+  },
+
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+    pageUpDown: true,
+  }
+});
